d17f6c329319022a3fc5fc170379bbe5fd93df70
Preeti Dhiman <pdhiman@enova.com>
2015-10-04 01:50:54 -0500

    rspec removed update booting file

--- overlord/features/booting.feature
+++ overlord/features/booting.feature
@@ -1,7 +1,23 @@
-Feature: Booting
+#Cucumber is a gem for ruby that executes plain-text functional descriptions as automated tests. 
+#The language that Cucumber understands is called Gherkin. Cucumber itself is written in Ruby.
+
+#This is a feature file covering scenarios for booting the bomb
+#Feature files have the suffix .feature and live in a directory named features.
+
+#Feature, Background, Scenario are defined in gherkin
+
+#A feature can contain a description (which is unparsed text) 
+#A background has a common step that is run if there are multiple scenarios
+#A scenario is comprised of steps. Each step is a short sentence that falls into one of the three familiar BDD categories:
+
+#Given (setting up a precondition for the scenario),
+#When (execute an action in the application under test) and
+#Then (assert the desired outcome)
+
+Feature: Booting the bomb
   As a super-villain
-  I want to boot a bomb
-  so that I can use it later
+  I do not want to activate the bomb when I first boot it with or without codes
+  So that I activate it later
 
   Background:
     Given my bomb is not booted
--- overlord/features/step_definitions/booting_steps.rb
+++ overlord/features/step_definitions/booting_steps.rb
@@ -1,23 +1,25 @@
-Given(/^my bomb is not booted$/) do
+#This is a step defination file.
+##Step def files have the suffix _steps.rb, though the name doesnt matter at all except, all .rb files in features/step_definations folder are loaded when running the features.
+
+#visit is a method from capybara that navigates to the given url
+Given /^my bomb is not booted$/ do
   visit("/")
 end
 
-When(/^I boot my bomb$/) do
+When /^I boot my bomb$/ do
   click_on("Boot")
 end
 
-Then(/^my bomb should be inactive$/) do
-  expect(find(".activation_status")).to have_content("INACTIVE")
+Then /^my bomb should be inactive$/ do
+  page.find(".activation_status").should have_content("INACTIVE")
 end
 
-Given(/^I set my activation code to (\d+)$/) do |code|
+#fill_in uses API to type a value into the text_box. 
+#The value is provided by what is captured by regex group. For example: if I say .....I set my activation code to "1234", it will fill in that value in the textbox
+Given /^I set my activation code to (\d+)$/ do |code|
   fill_in("activation_code", with: code)
 end
 
-Given(/^I set my deactivation code to (\d+)$/) do |code|
+Given /^I set my deactivation code to (\d+)$/ do |code|
   fill_in("deactivation_code", with: code)
-end
-
-When(/^I set my activation code to "(.*?)"$/) do |code|
-  fill_in("activation_code", with: code)
 end
