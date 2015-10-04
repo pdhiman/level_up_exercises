72bb272b6f775099429d1f63f466fddaa575bd00
Preeti Dhiman <pdhiman@enova.com>
2015-10-04 03:37:05 -0500

    excercises

--- overlord/features/step_definitions/booting_steps.rb
+++ overlord/features/step_definitions/booting_steps.rb
@@ -1,21 +1,22 @@
-#This is a step defination file.
+#This is a step definition file.
 ##Step def files have the suffix _steps.rb, though the name doesnt matter at all except, all .rb files in features/step_definations folder are loaded when running the features.
 
 #visit is a method from capybara that navigates to the given url
 Given /^my bomb is not booted$/ do
-  visit("/")
+  pending  #lets do this together
 end
 
 When /^I boot my bomb$/ do
-  click_on("Boot")
+  pending  #lets do this together
 end
 
 #fill_in uses API to type a value into the text_box. 
 #The value is provided by what is captured by regex group. For example: if I say .....I set my activation code to "1234", it will fill in that value in the textbox
 Given /^I set my activation code to (\d+)$/ do |code|
-  fill_in("activation_code", with: code)
+  pending #lets do this together
 end
 
 Given /^I set my deactivation code to (\d+)$/ do |code|
-  fill_in("deactivation_code", with: code)
+  #After we finishd  writing step def above, what do you think this should be?
+  pending #its pending. Yes, you can use that. 
 end
