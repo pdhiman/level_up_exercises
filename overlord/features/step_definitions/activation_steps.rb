dcfb1be7e6b6f7323a7661595e7a7c184948ae86
Preeti Dhiman <pdhiman@enova.com>
2015-10-05 16:56:27 -0500

    more updates

--- overlord/features/step_definitions/activation_steps.rb
+++ overlord/features/step_definitions/activation_steps.rb
@@ -6,23 +6,22 @@
  end
  
  When /^I enter the (?:code|time) (\d+)$/ do |code|
-   enter_code_on_keypad(code)
+   pending  #lets do this together
  end
  
  Then /^I should see the time prompt$/ do
-   expect(find(".message")).to have_content("TIMER")
+   pending  #lets do this together
  end
  
  Then /^the bomb should be activated$/ do
-   expect(find(".activation_status")).to have_content("ACTIVE")
+   pending  #lets do this together
  end
  
  #calling a step def in a step def
  Given /^the bomb is already activated$/ do
-   step 'I enter the code 1234'
-   step 'I enter the time 0400'
+   pending  #lets do this together
  end
  
  Then /^I should see the message "([^"]+)"$/ do |message|
-   expect(find(".message")).to have_content(message)
+   pending  #lets do this together
  end
